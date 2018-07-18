025faa738246356dd950c67a77d473d24820bbc0
Denny <dvriesman@gmail.com>
2018-07-17 22:48:11 -0300

    Setup project

---
+++ src/main/java/com/github/dvriesman/bitsotrade/MainApplication.java
@@ -0,0 +1,21 @@
+package com.github.dvriesman.bitsotrade;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class MainApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/github/dvriesman/bitsotrade/view/Application.fxml"));
+        primaryStage.setTitle("Bitsontrade");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) { launch(args); }
+}
---
+++ src/main/java/com/github/dvriesman/bitsotrade/controller/MainApplicationController.java
@@ -0,0 +1,21 @@
+package com.github.dvriesman.bitsotrade.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainApplicationController implements Initializable {
+
+    @FXML
+    private Button btTest;
+
+    public void initialize(URL location, ResourceBundle resourceBundle) {}
+
+    @FXML
+    private void btTestOnClick() {
+    }
+
+}
